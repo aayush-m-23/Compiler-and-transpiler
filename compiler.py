0f9ad59304653f219f4e756ff6fba3cbf6bb31ae
aayush-m-23 <aayush232mehra@gmail.com>
2025-05-28 09:17:19 +0530

    Update compiler.py

--- compiler.py
+++ compiler.py
@@ -178,4 +178,66 @@ class Parser:
         if token[0] in ('NUMBER', 'IDENTIFIER'):
             return token
         raise SyntaxError(f"Expected number or identifier, got {token[0]}")
+    
+class Interpreter:
+    def __init__(self, statements):
+        self.statements = statements
+        self.env = {}
+
+    def eval_expr(self, expr):
+        if isinstance(expr, tuple) and expr[0] in ('+', '-', '*', '/', '%', '>', '<'):
+            op, left, right = expr
+            lval = self.eval_expr(left)
+            rval = self.eval_expr(right)
+            if op == '+': return lval + rval
+            if op == '-': return lval - rval
+            if op == '*': return lval * rval
+            if op == '/': return lval // rval if rval != 0 else 0
+            if op == '%': return lval % rval if rval != 0 else 0
+            if op == '>': return lval > rval
+            if op == '<': return lval < rval
+        elif expr[0] == 'NUMBER':
+            return expr[1]
+        elif expr[0] == 'IDENTIFIER':
+            var = expr[1]
+            if var in self.env:
+                return self.env[var]
+            else:
+                raise NameError(f"Undefined variable '{var}'")
+        else:
+            return expr
+
+    def exec(self):
+        self.execute_statements(self.statements)
+
+    def execute_statements(self, statements):
+        for stmt in statements:
+            self.execute(stmt)
+
+    def execute(self, stmt):
+        if stmt[0] == 'ASSIGN':
+            _, name, expr = stmt
+            self.env[name] = self.eval_expr(expr)
+        elif stmt[0] == 'PRINT':
+            _, expr = stmt
+            print(self.eval_expr(expr))
+        elif stmt[0] == 'IF':
+            _, cond, true_branch, false_branch = stmt
+            if self.eval_expr(cond):
+                self.execute_statements(true_branch)
+            else:
+                self.execute_statements(false_branch)
+        elif stmt[0] == 'WHILE':
+            _, cond, body = stmt
+            while self.eval_expr(cond):
+                self.execute_statements(body)
+        elif stmt[0] == 'FOR':
+            _, var, start_expr, end_expr, body = stmt
+            start = self.eval_expr(start_expr)
+            end = self.eval_expr(end_expr)
+            for i in range(start, end + 1):
+                self.env[var] = i
+                self.execute_statements(body)
+
+
 
